a95afd91691de11fce4e7651c618d22b2b474f5c
mohamed4mahmouud <mohamedalamrawy55@gmail.com>
2024-01-15 23:59:49 +0200

    auth

--- javaScript/auth.js
+++ javaScript/auth.js
@@ -1,4 +1,6 @@
 let cartViewButton = document.querySelector("#cart-view");
+let logOutButton = document.querySelector(".logout-btn");
+let users = JSON.parse(localStorage.getItem("usersData"));
 
 cartViewButton.addEventListener("click", function () {
   if (localStorage.getItem("usresData") != null) {
@@ -7,3 +9,24 @@ cartViewButton.addEventListener("click", function () {
     alert("login");
   }
 });
+
+logOutButton.addEventListener("click", logOut);
+
+function logOut() {
+  let userIndex = findLoggedInUserIndex();
+
+  if (userIndex !== -1) {
+    users[userIndex].isLogged = false;
+    localStorage.setItem("usersData", JSON.stringify(users));
+  } else {
+  }
+}
+
+function findLoggedInUserIndex() {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].isLogged === true) {
+      return i;
+    }
+  }
+  return -1; // Return -1 if no user is found with isLogged set to true
+}
